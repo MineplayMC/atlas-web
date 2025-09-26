0879aed259f9b02fc38eb52bfd6b290ee2521b37
Esmaybe <me@esmay.be>
2025-09-26 17:38:00 +0200

    Fix 2

--- src/server/lib/auth.ts
+++ src/server/lib/auth.ts
@@ -26,6 +26,7 @@ function createAuth() {
       accountLinking: {
         enabled: true,
         trustedProviders: ["identity"],
+        allowDifferentEmails: true,
       },
     },
     plugins: [
